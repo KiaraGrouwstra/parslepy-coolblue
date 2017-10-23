aa34f4cee465d829accc1425d07a206805b961ff
Tycho Grouwstra <tychogrouwstra@gmail.com>
2017-10-23 19:32:57 +0200

    simplify

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Usage:
 ```bash
-scrapy crawl Parsley -o results/nytimes.jl -a parselet="`cat parselets/nytimes.yml`" -a item_key='items' -a domain='nytimes.com' -a url='http://www.nytimes.com/pages/technology/'
+scrapy crawl Parsley -o results/nytimes.jl -a parselet="`cat parselets/nytimes.yml`" -a domain='nytimes.com' -a url='http://www.nytimes.com/pages/technology/'
 # -a domain='www.coolblue.nl'
 ```
--- spiders/parsley.py
+++ spiders/parsley.py
@@ -3,35 +3,7 @@ import cStringIO
 import json
 import yaml
 from scrapy import Spider
-from scrapy.item import Item, Field
-from scrapy.loader import ItemLoader
-from scrapy.loader.processors import TakeFirst
 from parslepy import Parselet
-
-class MyItemLoader(ItemLoader):
-    '''scrapy item loader'''
-    default_output_processor = TakeFirst()
-
-def get_item_class(props):
-    '''get an item class for the given keys'''
-    class ItemCls(Item):
-        '''item class with dynamic properties'''
-        fields = {k : Field() for k in props}
-    cls = ItemCls()
-    for k in props:
-        cls._values[k] = Field()
-    return cls
-
-def parselet_keys(parselet):
-    '''get the item keys from a parselet dict'''
-    if isinstance(parselet, list):
-        return parselet_keys(parselet[0])
-    else:
-        keys = parselet.keys()
-        if keys[0][-1] == ')':
-            return parselet_keys(parselet.values()[0])
-        else:
-            return keys
 
 class ParsleySpider(Spider):
     '''scrape a parselet'''
@@ -39,11 +11,8 @@ class ParsleySpider(Spider):
 
     def __init__(self, **kwargs):
         super(ParsleySpider, self).__init__(**kwargs)
-        self.item_key = kwargs['item_key']
         dic = yaml.load(kwargs['parselet'])
         self.parselet = Parselet.from_jsonstring(json.dumps(dic))
-        keys = parselet_keys(dic)
-        self.item_cls = get_item_class(keys)
         domain = kwargs['domain']
         self.allowed_domains = [domain]
         url = kwargs.get('url', 'https://{}/'.format(domain))
@@ -51,15 +20,12 @@ class ParsleySpider(Spider):
 
     def parse(self, response):
         # self.logger.debug('crawled url {}'.format(response.request.url))
-        loader = MyItemLoader(self.item_cls)
         data = self.parselet.parse(cStringIO.StringIO(response.body))
-        if self.item_key:
-            for item_value in data.get(self.item_key):
-                loader.add_value(None, item_value)
-                yield loader.load_item()
+        if len(data.keys()) == 1 and isinstance(data.values()[0], list):
+            for item_value in data.values()[0]:
+                yield item_value
         else:
-            loader.add_value(None, data)
-            yield loader.load_item()
+            yield data
         # for link in scrapy.linkextractors.lxmlhtml.LxmlParserLinkExtractor().extract_links(response):
         #     yield scrapy.http.Request(link.url, callback=self.parse)
         # yield vars(response)
